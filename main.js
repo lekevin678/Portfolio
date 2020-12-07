fff1c3540774878102a0f373d197816a92694a69
lekevin678 <lekevin678@gmail.com>
2020-12-07 11:30:50 -0800

    added Procfile

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.set('port', process.argv[2]);
+app.set('port', process.env.PORT || 5000);
 
 app.use('/', express.static('public'));
 
